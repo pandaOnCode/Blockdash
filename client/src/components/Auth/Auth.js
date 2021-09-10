06b341cb0a3ef600ab6a8024c4f1feedca4fb333
pandaOnCode <asing111@ucsc.edu>
2021-09-10 16:05:19 -0700

    complete google oauth

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,22 +1,51 @@
-import React from 'react'
-import { Avatar, Button, Paper, Grid, Typography, Container, TextField, Input } from '@material-ui/core';
+import React, { useState } from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import { GoogleLogin } from 'react-google-login';
+import { useDispatch } from 'react-redux';
+import Icon from './Icon';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import useStyles from './styles';
-
+import Input from './Input';
+import { useHistory } from 'react-router-dom';
 const Auth = () => {
 
     const classes = useStyles();
-    const isSignup = false;
+    const [showPassword, setShowPassword] = useState(false);
+    const [isSignup, setIsSignup] = useState(false);
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+
     const handleSubmit = () => {
 
     };
     const handleChange = () => {
 
     }
+    const switchMode = () => {
+        setIsSignup((prevIsSignup) => !prevIsSignup);
+        handleShowPassword(false);
+    }
+
+    const googleSuccess = async (res) => {
+        const result = res?.profileObj; //undefined
+        const token = res?.tokenId;
+        try {
+            dispatch({ type: 'AUTH', data: { result, token } });
+            history.push('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const googleFailure = (error) => {
+        console.log(error);
+        console.log('Something is not right. Try again Later');
+    }
+
     return (
         <Container component="main" maxWidth="xs">
             <Paper className={classes.paper} elevation={3}>
-                <Avatar classNAME={classes.avatar}>
+                <Avatar className={classes.avatar}>
                     <LockOutlinedIcon />
                 </Avatar>
                 <Typography variant="h5">{isSignup ? 'Sign up' : 'Sign In'}</Typography>
@@ -26,11 +55,37 @@ const Auth = () => {
                             isSignup && (
                                 <>
                                     <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
-                                    <Input name="firstName" label="First Name" handleChange={handleChange} half />
+                                    <Input name="lastName" label="Last Name" handleChange={handleChange} half />
                                 </>
                             )
                         }
                         <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
+                        <Input name="password" label="password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+                        {isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" />}
+                    </Grid>
+
+
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Button onClick={switchMode}>
+                                {isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up"}
+                            </Button>
+                            <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+                                {isSignup ? 'Sign Up' : 'Sign In'}
+                            </Button>
+                            <GoogleLogin
+                                clientId="1042808790275-tvlrk8vel3lbpds5oen3vuhumugoj0qr.apps.googleusercontent.com"
+                                render={(renderProps) => (
+                                    <Button className={classes.googleButton} color='primary' fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon />} variant='contained'>
+                                        Sign in with Google
+                                    </Button>
+                                )}
+                                onSuccess={googleSuccess}
+                                onFailiure={googleFailure}
+                                cookiePolicy="single_host_origin"
+                            />
+
+                        </Grid>
                     </Grid>
                 </form>
             </Paper>
--- client/src/components/Auth/Input.js
+++ client/src/components/Auth/Input.js
@@ -2,12 +2,12 @@ import React from 'react'
 import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core'
 import Visibility from '@material-ui/icons/Visibility'
 import VisibilityOff from '@material-ui/icons/Visibility'
-import Input from './Input';
+
 
 const Input = ({ name, handleChange, label, half, autoFocus, type, handleShowPassword }) => {
     return (
         <Grid item xs={12} sm={half ? 6 : 12}>
-            <TextField name={name} onChange={handleChange} variant="outlined" required fullWidth label={label} autoFocus={autoFocus} type={type} InputProps={name === 'password' && {
+            <TextField name={name} onChange={handleChange} variant="outlined" required fullWidth label={label} autoFocus={autoFocus} type={type} InputProps={name === 'password' ? {
                 endAdornment: (
                     <InputAdornment position="end">
                         <IconButton onClick={handleShowPassword}>
@@ -15,7 +15,7 @@ const Input = ({ name, handleChange, label, half, autoFocus, type, handleShowPas
                         </IconButton>
                     </InputAdornment>
                 ),
-            }} />
+            } : null} />
         </Grid >
     )
 }
